5ddd533bd618f25140317cf851dc1a8cbb035bee
Holger Eiboeck <holger@eiboeck.de>
2026-04-27 18:40:30 +0200

    Clean up proto.h: remove duplicates and unimplemented declarations

--- src/proto.h
+++ src/proto.h
@@ -1,54 +1,22 @@
-//
-// Created by Holger Eiboeck on 31.10.17.
-//
-
-#ifndef WETTERSTATION_PROTO_H
-#define WETTERSTATION_PROTO_H
+#pragma once
 
 bool shouldStartSetup();
-
 void doSetup();
-
 void setupWIFI();
-
 void setupWebserver();
-
 void handleNotFound();
-
-void getRemoteTempHumi(byte *data);
-
 void displayData();
-
 void setReadyForInternalSensorUpdate();
-
 void setReadyForTimeUpdate();
-
 double berechneTT(double t, double RH);
-
 void setExternalSensorInvalid();
-
-bool shouldStartSetup();
-
 void updateInternalSensor();
-
-void setExternalSensorInvalid();
-
 void updateTime();
-
 double RHtoDP(double t, double RH);
-
-boolean loadConfig();
-
 void flash();
-
 void saveConfigCallback();
-
 void configModeCallback(WiFiManager *myWiFiManager);
-
 void handleRoot();
-
 void handleJsonData();
-
 void updateExternalSensor();
 
-#endif //WETTERSTATION_PROTO_H
