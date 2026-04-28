f477a7bdf61274f798f435be995cb96ab1135d97
Holger Eiboeck <holger@eiboeck.de>
2026-04-28 09:10:44 +0200

    More tests

---
+++ test/test_humidity/test_main.cpp
@@ -0,0 +1,32 @@
+#include <unity.h>
+#include "HumidityMath.h"
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_absolute_humidity_known_room_value(void) {
+    TEST_ASSERT_FLOAT_WITHIN(0.05f, 8.64f, berechneTT(20.0, 50.0));
+}
+
+void test_dewpoint_known_room_value(void) {
+    TEST_ASSERT_FLOAT_WITHIN(0.05f, 9.26f, RHtoDP(20.0, 50.0));
+}
+
+void test_absolute_humidity_increases_with_relative_humidity(void) {
+    TEST_ASSERT_GREATER_THAN(berechneTT(20.0, 40.0), berechneTT(20.0, 60.0));
+}
+
+void test_dewpoint_increases_with_temperature_at_same_relative_humidity(void) {
+    TEST_ASSERT_GREATER_THAN(RHtoDP(10.0, 50.0), RHtoDP(20.0, 50.0));
+}
+
+int main(int argc, char **argv) {
+    (void) argc;
+    (void) argv;
+    UNITY_BEGIN();
+    RUN_TEST(test_absolute_humidity_known_room_value);
+    RUN_TEST(test_dewpoint_known_room_value);
+    RUN_TEST(test_absolute_humidity_increases_with_relative_humidity);
+    RUN_TEST(test_dewpoint_increases_with_temperature_at_same_relative_humidity);
+    return UNITY_END();
+}
