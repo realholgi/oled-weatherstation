f1693d7d8e057899e2992f0aef83c74308edf0a9
Holger Eiboeck <holger@eiboeck.de>
2026-04-29 20:50:27 +0200

    Extract timezones

--- src/Wifi.cpp
+++ src/Wifi.cpp
@@ -7,49 +7,7 @@
 #include "Display.h"
 #include "ConfigStore.h"
 #include "config.h"
-
-struct TzEntry { const char *name; const char *posix; };
-
-static const TzEntry TIMEZONES[] = {
-    // Americas
-    {"HST-10  – Honolulu",           "HST10"},
-    {"AKST-9  – Anchorage",          "AKST9AKDT,M3.2.0,M11.1.0"},
-    {"PST-8   – Los Angeles",        "PST8PDT,M3.2.0,M11.1.0"},
-    {"MST-7   – Denver",             "MST7MDT,M3.2.0,M11.1.0"},
-    {"MST-7   – Phoenix (no DST)",   "MST7"},
-    {"CST-6   – Chicago",            "CST6CDT,M3.2.0,M11.1.0"},
-    {"EST-5   – New York",           "EST5EDT,M3.2.0,M11.1.0"},
-    {"ART-3   – Buenos Aires",       "ART3"},
-    {"BRT-3   – Sao Paulo",          "BRT3"},
-    // Europe
-    {"WET     – Lisbon",             "WET0WEST,M3.5.0/1,M10.5.0"},
-    {"GMT     – London / Dublin",    "GMT0BST,M3.5.0/1,M10.5.0"},
-    {"CET+1   – Berlin / Paris",     "CET-1CEST,M3.5.0,M10.5.0/3"},
-    {"EET+2   – Athens / Helsinki",  "EET-2EEST,M3.5.0/3,M10.5.0/4"},
-    {"MSK+3   – Moscow",             "MSK-3"},
-    // Africa / Middle East
-    {"WAT+1   – Lagos",              "WAT-1"},
-    {"CAT+2   – Johannesburg",       "SAST-2"},
-    {"EAT+3   – Nairobi",            "EAT-3"},
-    {"AST+3   – Riyadh",             "AST-3"},
-    {"GST+4   – Dubai",              "GST-4"},
-    // Asia
-    {"IST+5:30 – Mumbai / Delhi",    "IST-5:30"},
-    {"BST+6   – Dhaka",              "BST-6"},
-    {"ICT+7   – Bangkok / Jakarta",  "ICT-7"},
-    {"CST+8   – Beijing / Shanghai", "CST-8"},
-    {"SGT+8   – Singapore",          "SGT-8"},
-    {"JST+9   – Tokyo",              "JST-9"},
-    {"KST+9   – Seoul",              "KST-9"},
-    // Oceania
-    {"AWST+8  – Perth",              "AWST-8"},
-    {"AEST+10 – Sydney / Melbourne", "AEST-10AEDT,M10.1.0,M4.1.0/3"},
-    {"NZST+12 – Auckland",           "NZST-12NZDT,M9.5.0,M4.1.0/3"},
-    // UTC
-    {"UTC",                          "UTC0"},
-};
-
-static const size_t TZ_COUNT = sizeof(TIMEZONES) / sizeof(TIMEZONES[0]);
+#include "Timezones.h"
 
 Display *Wifi::activeDisplay = nullptr;
 AppConfig *Wifi::activeConfig = nullptr;
